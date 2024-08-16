284c3641603ff53c69c0c1c11f2b3a7378a0ad03
Mamush-Meshesha <mamgithub@gmail.com>
2024-08-16 08:41:00 +0300

    project structure

---
+++ components/header.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+<template>
+   <div class="bg-white h-20 fixed z-50 w-full">
+     <div class="container mx-auto  h-full  ">
+        <div class="flex justify-between items-center h-full ">
+            <h1 class="text-3xl font-serif font-bold">Mamush Meshesha</h1>
+            <div class="flex gap-3 items-center text-2xl">
+                <h1>Home</h1>
+                <h1>About</h1>
+                <h1>Projects</h1>
+                <button>Contact Me</button>
+            </div>
+        </div>
+    </div>
+   </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,63 @@
+<template>
+<div>
+    <div class="bg-[#1d204e]">
+        <div class="back fixed top-0 right-0 left-0 bottom-0 z-0 backdrop:blur opacity-20"></div>
+        <div class="container mx-auto relative h-screen">
+            <div class="flex justify-center items-center h-full">
+                <div class="leading-[70px]">
+                    <h1 class="text-7xl capitalize font-serif font-bold py-3 text-white">
+                        Full stack web developer
+                    </h1>
+                    <p class="text-2xl text-white text-center">
+                        Showcasing projects and skills with stunning animations
+                    </p>
+                    <div class="flex justify-center py-6">
+                        <button class="bg-[#32a927] h-[50px] flex items-center px-6 text-white rounded-md">
+                            View Projects
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- about section -->
+
+    <div class="min-h-screen  z-10 bg-white relative">
+        <div class="container mx-auto">
+            <div class="grid grid-cols-2 gap-10 w-full h-full  items-center">
+                <div class="w-[80%] ">
+                    <div>
+                        <h1 class="text-4xl font-bold font-serif">Passionate fullstack developer</h1>
+                        <p class="text-2xl py-3 leading-9 text-[#595959]">
+                            I am a dedicated fullstack developer based in Addis Ababa,
+                            specializing in creating dynamic web applications. With expertise in
+                            Nuxt 3 and Tailwind CSS, I craft responsive and engaging user
+                            experiences that showcase my projects, skills, and professional
+                            journey. My portfolio reflects my commitment to excellence and
+                            innovation in every line of code I write.
+                        </p>
+                        <button>Get in touch</button>
+                    </div>
+                </div>
+                <div class="flex h-full items-center">
+                    <img src="@/assets/about.jpeg" alt="" class="h-[70%] object-cover rounded-md">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+import image from "@/assets/pc.jpg";
+</script>
+
+<style scoped>
+.back {
+    background-image: url("@/assets/pc1.png");
+    background-size: cover;
+    /* Optional: Adjusts the background image size */
+    height: 100vh;
+}
+</style>
